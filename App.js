32218922fa087a0bc39370aa4d12eb9afd81c415
mariaclarabarbosa <clara_estrella@protonmail.com>
2020-10-07 19:18:11 -0300

    Projeto funcionando com o splashscreen estático

--- App.js
+++ App.js
@@ -1,16 +1,47 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/navegacoes/StackNavigation'
 import DrawerNavigation from './src/navegacoes/DrawerNavigation'
+import LottieView from "lottie-react-native";
 
 
 export default function App() {
+  // const [isReady, setIsReady] = useState(true);
+  // const animation = useRef(null);
+
+  // useEffect(() => {
+  //   if (!animation.current) {
+  //     console.log("oi");
+  //     return;
+  //   }
+  //   animation.current.play();
+  // }, [animation]);
+  
+  // if (!isReady) {
+  //   return (
+  //     <LottieView
+  //       ref={animation}
+  //       style={{
+  //         width: 400,
+  //         height: 400,
+  //         backgroundColor: "#eee",
+  //       }}
+  //       loop={false}
+  //       speed={2}
+  //       source={require("./assets/animation/splashscreen.json")}
+  //       onAnimationFinish={() => setIsReady(true)}
+  //     />
+  //   );
+  // }
   return (
-    <NavigationContainer>
-      <StackNavigation>
-        <DrawerNavigation />
-      </StackNavigation>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <StackNavigation>
+          <DrawerNavigation />
+        </StackNavigation>
+      </NavigationContainer>
+    </>
   );
+  
 }
